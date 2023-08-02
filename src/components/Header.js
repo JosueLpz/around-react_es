303ad575c6bbd60f889ae80a26dbc906bb916d4a
unknown <josuelpzti@gmail.com>
2023-08-02 09:57:46 -0400

    apis 50%

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
+import { useState, useEffect } from "react";
 import header__logo from "../image/header/header__logo.svg";
 import editButton from "../image/header/EditButton.svg";
 import addButton from "../image/nav/Addbutton.svg";
+import api from "../utils/api.js";
 
 function Header(props) {
+  const [userName, setUserName] = useState("");
+  const [userDescription, setUserDescription] = useState("");
+  const [userAvatar, setUserAvatar] = useState("");
+
+  useEffect(() => {
+    api.getInfoServer("users/me").then((data) => {
+      setUserAvatar(data.avatar);
+      setUserDescription(data.about);
+      setUserName(data.name);
+    });
+  }, []);
+
   return (
     <>
       <header className="header">
@@ -10,16 +24,16 @@ function Header(props) {
         <div className="header__line"></div>
       </header>
       <nav className="profile">
-        <div onClick={props.onEditAvatarClick} className="profile__content-img">
+        <div onClick={props.onEditAvatarClick} className="profile__content-img" style={{ backgroundImage: `url(${userAvatar})` }}>
           <img className="profile__img profile__img-hover" name="avatar" alt="avatarusuario" />
         </div>
         <div className="profile__row">
-          <h1 className="profile__row-name"></h1>
+          <h1 className="profile__row-name">{userName}</h1>
           <a onClick={props.onEditProfileClick} className="profile__row-edit root__button-hover-active">
             <img src={editButton} alt="botonedit" />
           </a>
         </div>
-        <h2 className="profile__hobbie"></h2>
+        <h2 className="profile__hobbie">{userDescription}</h2>
         <a onClick={props.onAddPlaceClick} className="profile__button root__button-hover-active">
           <img className="profile__button" src={addButton} alt="BotonAdd" />
         </a>
