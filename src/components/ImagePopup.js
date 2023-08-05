f6d0efb403080768277d6c43c51235450bbc6bad
unknown <josuelpzti@gmail.com>
2023-08-05 09:46:10 -0400

    done

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -3,9 +3,9 @@ import closeIcon from "../image/nav/CloseIcon.svg";
 export default function ImagePopup(props) {
   return (
     <div className="zoom">
-      <a className="zoom__button-closed root__buttom-closed-active" onClick={props.onClose}>
+      <button className="zoom__button-closed root__buttom-closed-active" onClick={props.onClose}>
         <img src={closeIcon} alt="ButtonClosed" />
-      </a>
+      </button>
       <div className="zoom__content"></div>
       <img className="zoom__img" src={props.card.link} />
       <p className="zoom__text">{props.card.name}</p>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,9 +5,9 @@ export default function PopupWithForm(props) {
     <>
       <fieldset className={`popup ${props.name}`}>
         <form className={`form__container popup__${props.name}`} name={props.name} noValidate>
-          <a onClick={props.handleClose} className="form__container-closed root__buttom-closed-active popup__closed">
+          <button onClick={props.handleClose} className="form__container-closed root__buttom-closed-active popup__closed">
             <img src={closeIcon} alt="ButtonClosed" />
-          </a>
+          </button>
           <h2 className="form__container-title">{props.title}</h2>
           {props.children}
           <button type="submit" className="form__container-button root__button-hover-active popup__button">
