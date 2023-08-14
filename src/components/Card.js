17502ce216819628b813535a11ea58d1e1d3978f
unknown <josuelpzti@gmail.com>
2023-08-14 08:32:43 -0400

    context 50%

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,32 @@
+import { useContext } from "react";
 import buttonDelete from "../image/element/ButtonDelete.svg";
 import buttonLike from "../image/element/ButonLike.svg";
+import { CurrentUserContext } from "./contexts/CurrentUserContext";
 
 export default function Card(props) {
   function handleClick() {
     props.onCardClick(props.data);
   }
 
+  const dataUser = useContext(CurrentUserContext);
+
+  const isOwn = props.data.owner._id === dataUser._id;
+
+  const cardDeleteButtonStyle = {
+    display: isOwn ? "block" : "none",
+  };
+
   return (
     <article className="element__article" key={props.data._id}>
-      <button className="element__article_delete root__button-hover-active">
+      <button type="button" className="element__article_delete root__button-hover-active" style={cardDeleteButtonStyle}>
         <img src={buttonDelete} alt="buttondelete" /*Boton de eliminar Card */ />
       </button>
-      <button className="element__article_img_button" onClick={handleClick}>
+      <button type="button" className="element__article_img_button" onClick={handleClick}>
         <img className="element__article_img" alt={props.data.name} src={props.data.link} />
       </button>
       <div className="element__article_row">
         <h2 className="element__article_row_title">{props.data.name}</h2>
-        <button className="element__article_row_like root__button-hover-active" /*Boton de Likes Card */>
+        <button type="button" className="element__article_row_like root__button-hover-active" /*Boton de Likes Card */>
           <img src={buttonLike} alt="buttonlike" />
         </button>
         <p className="element__article_row_like_counter">{props.data.likes.length}</p>
