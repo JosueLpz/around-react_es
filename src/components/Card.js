4c071aca9668d50f2367d0cd39ea81e0582012a0
unknown <josuelpzti@gmail.com>
2023-09-01 08:58:52 -0400

    main elevado

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,7 @@
-import { useContext } from "react";
 import buttonDelete from "../image/element/ButtonDelete.svg";
 import buttonLike from "../image/element/ButonLike.svg";
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 export default function Card(props) {
-  const dataUser = useContext(CurrentUserContext);
   // recuerda enviar los datos a otro componente atraves de una funcion y su parametro!
   function handleZoomClick() {
     props.onCardClick(props.card);
@@ -17,14 +14,14 @@ export default function Card(props) {
   }
 
   // forma 1, con un objeto, para desplegar el dysplay
-  const isOwn = props.card.owner._id === dataUser._id;
+  const isOwn = props.card.owner._id === props.userData._id;
   const cardDeleteButtonStyle = {
     display: isOwn ? "block" : "none",
   };
 
   // forma 2 con clases para modificar el elemento
   const isLiked = props.card.likes.some((like) => {
-    return like._id === dataUser._id;
+    return like._id === props.userData._id;
   });
   const cardLikeButtonStyle = `element__article_row_like root__button-hover-active ${
     isLiked ? "element__article_row_like_active" : "element__article_row_like"
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,21 @@
+import React, { useContext } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import Card from "./Card.js";
 
 function Main(props) {
+  const userData = useContext(CurrentUserContext);
   return (
     <>
       <main className="element">
         {props.cards.map((card) => (
-          <Card key={card._id} card={card} onCardClick={props.onCardClick} onCardLike={props.onCardLike} onCardDelete={props.onCardDelete} />
+          <Card
+            key={card._id}
+            card={card}
+            onCardClick={props.onCardClick}
+            onCardLike={props.onCardLike}
+            onCardDelete={props.onCardDelete}
+            userData={userData}
+          />
         ))}
       </main>
     </>
